a21b5dbdf4a4d5586ee698011fafb2a5ce9735ce
Jan Uthoff <JanUthoff@Uttis-MBP.local>
2017-01-04 15:28:00 +0100

    increased window size

--- Projects/rfid_reader_writer/GUI/main.cpp
+++ Projects/rfid_reader_writer/GUI/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.setFixedWidth(600);
-    w.setFixedHeight(550);
+    w.setFixedHeight(650);
     w.show();
 
     return a.exec();
