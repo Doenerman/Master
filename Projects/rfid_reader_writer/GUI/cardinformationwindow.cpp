f47d4090cbde138ae31a35b07d0cd86e4afa0b1b
Jan Uthoff <JanUthoff@Uttis-MBP.local>
2017-01-04 15:17:49 +0100

    Added new windows A new QMainWindow that display the information of the current card. the window can be closed via a button. the refresh button is clickable but nothing will be reloaded (not implemented yet).

---
+++ Projects/rfid_reader_writer/GUI/cardinformationwindow.cpp
@@ -0,0 +1,193 @@
+#include "cardinformationwindow.h"
+
+
+CardInformationWindow::CardInformationWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+
+    // Set the properties of the different objects
+    // Properties:  - geometry (including the placement)
+    //              - object name
+    //              - possible text
+    //              - possible placeholder if no entry is given
+    //              - whether the object is enabled
+    //              - whether the user can overwrite given text
+
+
+    // ########### //
+    // ## Label ## //
+    // ########### //
+    lUniquecardID->setObjectName(QString::fromUtf8("label_uniqueID"));
+    lUniquecardID->setGeometry(QRect(
+                                     LABEL_X_POS_FIRST_COLUMN,
+                                     LABEL_Y_POS_UNIQUEID,
+                                     2*LABEL_DEFAULT_WIDTH,
+                                     LABEL_DEFAULT_HEIGHT));
+    lUniquecardID->setText(QString::fromUtf8("eindeutige Karten Nummer"));
+
+    lLocationNumber->setObjectName(QString::fromUtf8("label_locationNumber"));
+    lLocationNumber->setGeometry(QRect(LABEL_X_POS_FIRST_COLUMN,
+                                       LABEL_Y_POS_LOCNR,
+                                       LABEL_DEFAULT_WIDTH,
+                                       LABEL_DEFAULT_HEIGHT));
+    lLocationNumber->setText(QString::fromUtf8("Landeskennung"));
+
+    lRev->setObjectName(QString::fromUtf8("label_revision"));
+    lRev->setGeometry(QRect(LABEL_X_POS_SECOND_COLUMN,
+                            LABEL_Y_POS_REVNR,
+                            LABEL_DEFAULT_WIDTH,
+                            LABEL_DEFAULT_HEIGHT));
+    lRev->setText(QString::fromUtf8("Revision Nummer"));
+
+    lUserID->setObjectName(QString::fromUtf8("label_userID"));
+    lUserID->setGeometry(QRect(LABEL_X_POS_FIRST_COLUMN,
+                               LABEL_Y_POS_USERID,
+                               LABEL_DEFAULT_WIDTH,
+                               LABEL_DEFAULT_HEIGHT));
+    lUserID->setText(QString::fromUtf8("Kundennummer"));
+
+    lCardID->setObjectName(QString::fromUtf8("label_cardID"));
+    lCardID->setGeometry(QRect(LABEL_X_POS_FIRST_COLUMN,
+                               LABEL_Y_POS_CARDID,
+                               LABEL_DEFAULT_WIDTH,
+                               LABEL_DEFAULT_HEIGHT));
+    lCardID->setText(QString::fromUtf8("Kartennummer"));
+
+    lChecksumAdded->setObjectName(QString::fromUtf8("label_checksumAdded"));
+    lChecksumAdded->setGeometry(QRect(LABEL_X_POS_FIRST_COLUMN,
+                                      LABEL_Y_POS_CHECKADD,
+                                      LABEL_DEFAULT_WIDTH,
+                                      LABEL_DEFAULT_HEIGHT));
+    lChecksumAdded->setText(QString::fromUtf8("Pruefsumme (addiert)"));
+
+    lChecksumIBM->setObjectName(QString::fromUtf8("label_checksumIBM"));
+    lChecksumIBM->setGeometry(QRect(LABEL_X_POS_FIRST_COLUMN,
+                                    LABEL_Y_POS_CHECKIBM,
+                                    LABEL_DEFAULT_WIDTH,
+                                    LABEL_DEFAULT_HEIGHT));
+    lChecksumIBM->setText(QString::fromUtf8("Preufsumme (IBM)"));
+
+    lMD5->setObjectName(QString::fromUtf8("label_MD5"));
+    lMD5->setGeometry(QRect(LABEL_X_POS_FIRST_COLUMN,
+                            LABEL_Y_POS_MD5,
+                            LABEL_DEFAULT_WIDTH,
+                            LABEL_DEFAULT_HEIGHT));
+    lMD5->setText(QString::fromUtf8("MD5"));
+
+
+
+    // ################### //
+    // ## PlainTextEdit ## //
+    // ################### //
+    pteUniquecardID->setObjectName(QString::fromUtf8("pte_uniqueCardID"));
+    pteUniquecardID->setEnabled(false);
+    pteUniquecardID->setGeometry(QRect(PLAINTEXTEDIT_X_FIRST_COLUMN,
+                                       LABEL_Y_POS_UNIQUEID
+                                       + PLAINTEXT_LABEL_PUFFER,
+                                       2*PLAINTEXTEDIT_DEFAULT_WIDTH,
+                                       PLAINTEXTEDIT_DEFAULT_HEIGHT));
+
+    pteLocationNumber->setObjectName(QString::fromUtf8("pte_locNr"));
+    pteLocationNumber->setEnabled(false);
+    pteLocationNumber->setGeometry(QRect(PLAINTEXTEDIT_X_FIRST_COLUMN,
+                                         LABEL_Y_POS_LOCNR
+                                         + PLAINTEXT_LABEL_PUFFER,
+                                         PLAINTEXTEDIT_DEFAULT_WIDTH,
+                                         PLAINTEXTEDIT_DEFAULT_HEIGHT));
+
+    pteRev->setObjectName(QString::fromUtf8("pte_revNr"));
+    pteRev->setEnabled(false);
+    pteRev->setGeometry(QRect(PLAINTEXTEDIT_X_SECOND_COLUMN,
+                              LABEL_Y_POS_REVNR
+                              + PLAINTEXT_LABEL_PUFFER,
+                              PLAINTEXTEDIT_DEFAULT_WIDTH,
+                              PLAINTEXTEDIT_DEFAULT_HEIGHT));
+
+    pteUserID->setObjectName(QString::fromUtf8("pte_userID"));
+    pteUserID->setEnabled(false);
+    pteUserID->setGeometry(QRect(PLAINTEXTEDIT_X_FIRST_COLUMN,
+                                 LABEL_Y_POS_USERID
+                                 + PLAINTEXT_LABEL_PUFFER,
+                                 PLAINTEXTEDIT_DEFAULT_WIDTH,
+                                 PLAINTEXTEDIT_DEFAULT_HEIGHT
+                                 ));
+
+    pteCardID->setObjectName(QString::fromUtf8("pte_cardID"));
+    pteCardID->setEnabled(false);
+    pteCardID->setGeometry(QRect(PLAINTEXTEDIT_X_FIRST_COLUMN,
+                                 LABEL_Y_POS_CARDID
+                                 + PLAINTEXT_LABEL_PUFFER,
+                                 PLAINTEXTEDIT_DEFAULT_WIDTH,
+                                 PLAINTEXTEDIT_DEFAULT_HEIGHT
+                                 ));
+
+    pteChecksumAdded->setObjectName(QString::fromUtf8("pte_checkAdd"));
+    pteChecksumAdded->setEnabled(false);
+    pteChecksumAdded->setGeometry(QRect(PLAINTEXTEDIT_X_FIRST_COLUMN,
+                                        LABEL_Y_POS_CHECKADD
+                                        + PLAINTEXT_LABEL_PUFFER,
+                                        PLAINTEXTEDIT_DEFAULT_WIDTH,
+                                        PLAINTEXTEDIT_DEFAULT_HEIGHT
+                                       ));
+
+    pteChecksumIBM->setObjectName(QString::fromUtf8("pte_checkIBM"));
+    pteChecksumIBM->setEnabled(false);
+    pteChecksumIBM->setGeometry(QRect(PLAINTEXTEDIT_X_FIRST_COLUMN,
+                                      LABEL_Y_POS_CHECKIBM
+                                      + PLAINTEXT_LABEL_PUFFER,
+                                      PLAINTEXTEDIT_DEFAULT_WIDTH,
+                                      PLAINTEXTEDIT_DEFAULT_HEIGHT
+                                     ));
+
+    pteMD5->setObjectName(QString::fromUtf8("pte_MD5"));
+    pteMD5->setEnabled(false);
+    pteMD5->setGeometry(QRect(PLAINTEXTEDIT_X_FIRST_COLUMN,
+                              LABEL_Y_POS_MD5
+                              + PLAINTEXT_LABEL_PUFFER,
+                              2*PLAINTEXTEDIT_DEFAULT_WIDTH,
+                              PLAINTEXTEDIT_DEFAULT_HEIGHT
+                             ));
+
+    // ################ //
+    // ## PushButton ## //
+    // ################ //
+    pbRefresh->setObjectName(QString::fromUtf8("refresh_button"));
+    pbRefresh->setGeometry(QRect(PUSHBOTTEN_X_REFRESH,
+                                 PUSHBOTTON_Y,
+                                 PUSHBUTTON_DEFAULT_WIDTH,
+                                 PUSHBOTTON_DEFAULT_HEIGHT));
+    pbRefresh->setText(QString::fromUtf8("Erneut Lesen"));
+
+    pbClose->setObjectName(QString::fromUtf8("close_button"));
+    pbClose->setGeometry(QRect(PUSHBOTTON_X_CLOSE,
+                               PUSHBOTTON_Y,
+                               PUSHBUTTON_DEFAULT_WIDTH,
+                               PUSHBOTTON_DEFAULT_HEIGHT));
+    pbClose->setText(QString::fromUtf8("Lesen beenden"));
+
+
+
+
+
+    // ######################### //
+    // ## Set the focus order ## //
+    // ######################### //
+    this->setTabOrder(pbRefresh, pbClose);
+
+
+    // ########################################### //
+    // ## Connections between SIGNALS and SLOTS ## //
+    // ########################################### //
+    connect(this->pbClose,SIGNAL(clicked(bool)),this,SLOT(close()));
+    connect(this->pbRefresh,SIGNAL(clicked(bool)),this,SLOT(reload()));
+}
+
+
+/**
+ * @brief   Initiate the reload of the information
+ *
+ * @todo implementation
+ */
+void CardInformationWindow::reload() {
+
+}
---
+++ Projects/rfid_reader_writer/GUI/cardinformationwindow.h
@@ -0,0 +1,169 @@
+#ifndef CARDINFORMATIONWINDOW_H
+#define CARDINFORMATIONWINDOW_H
+
+#include <QMainWindow>
+#include <QPlainTextEdit>
+#include <QLabel>
+#include <QPushButton>
+
+#include "eventhandler.h"
+
+
+// ################################### //
+// ## Defines for the window layout ## //
+// ################################### //
+#define LABEL_DEFAULT_WIDTH 150
+#define LABEL_DEFAULT_HEIGHT 30
+#define LABEL_X_POS_FIRST_COLUMN 20
+#define LABEL_X_POS_SECOND_COLUMN 170
+#define LABEL_Y_POS_UNIQUEID 30
+#define LABEL_Y_POS_LOCNR 100
+#define LABEL_Y_POS_REVNR 170
+#define LABEL_Y_POS_USERID 240
+#define LABEL_Y_POS_CARDID 310
+#define LABEL_Y_POS_CHECKADD 380
+#define LABEL_Y_POS_CHECKIBM 450
+#define LABEL_Y_POS_MD5 520
+#define PUSHBUTTON_DEFAULT_WIDTH 150
+#define PUSHBOTTON_DEFAULT_HEIGHT 50
+#define PUSHBOTTEN_X_REFRESH 50
+#define PUSHBOTTON_X_CLOSE 200
+#define PUSHBOTTON_Y 600
+#define PLAINTEXT_LABEL_PUFFER 25
+#define PLAINTEXTEDIT_DEFAULT_WIDTH 150
+#define PLAINTEXTEDIT_DEFAULT_HEIGHT 30
+#define PLAINTEXTEDIT_X_FIRST_COLUMN LABEL_X_POS_FIRST_COLUMN
+#define PLAINTEXTEDIT_X_SECOND_COLUMN LABEL_X_POS_SECOND_COLUMN
+
+
+class CardInformationWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit CardInformationWindow(QWidget *parent = 0);
+
+signals:
+
+private:
+
+    // ########### //
+    // ## Label ## //
+    // ########### //
+    /**
+      * @var lUniquecardID
+      * QLabel to inform the user that the QPlainTextEdit below shows the
+      * unique card ID
+      */
+    QLabel* lUniquecardID = new QLabel(this);
+    /**
+      * @var lLocationNumber
+      * QLabel to inform the user that the QPLainTextEdit below shows the
+      * location number
+      */
+    QLabel* lLocationNumber = new QLabel(this);
+/** @todo add missing label for the information of the first block */
+    /**
+      * @var lRev
+      * QLabel to inform the user that the QPlainTextEdit below shows the
+      * Revision number
+      */
+    QLabel* lRev = new QLabel(this);
+    /**
+      * @var lUserID
+      * QLabel to inform the user that the QPlainTextEdit below shows the
+      * user ID of the card
+      */
+    QLabel* lUserID = new QLabel(this);
+    /**
+      * @var lCardId
+      * QLabel to inform the user that the QPLainTextEdit below shows the
+      * card ID
+      */
+    QLabel* lCardID = new QLabel(this);
+    /**
+      * @var lChecksumAdded
+      * QLabel to inform the user that the QPlainTextEdit below shows the
+      * added checksum of the card
+      */
+    QLabel* lChecksumAdded = new QLabel(this);
+    /**
+      * @var lChecksumIBM
+      * QLabel to inform the user that the QPlainTextEdit below shows the
+      * IBM checksum
+      */
+    QLabel* lChecksumIBM = new QLabel(this);
+    /**
+      * @var lMD5
+      * QLabel to inform the user that the QPlainTextEdit below shows the
+      * MD5 checksum of the card
+      */
+    QLabel* lMD5 = new QLabel(this);
+
+
+    // ################### //
+    // ## PlainTextEdit ## //
+    // ################### //
+    /**
+      * @var pteUniquecardID
+      * A QPlainTextEdit field for the output of the read unique card ID
+      */
+    QPlainTextEdit* pteUniquecardID = new QPlainTextEdit(this);
+    /**
+      * @var pteLocationNumber
+      * A QPlainTextEdit field for the output of the read location number
+      */
+    QPlainTextEdit* pteLocationNumber = new QPlainTextEdit(this);
+/** @todo add missing information of the first block */
+    /**
+      * @var pteRev
+      * A QPlainTextEdit field for the output of the read revision number
+      */
+    QPlainTextEdit* pteRev = new QPlainTextEdit(this);
+    /**
+      * @var pteUserID
+      * A QPlainTextEdit field for the output of the read user ID
+      */
+    QPlainTextEdit* pteUserID = new QPlainTextEdit(this);
+    /**
+      * @var pteCardID
+      * A QPlainTextEdit field for the output of the read card ID
+      */
+    QPlainTextEdit* pteCardID = new QPlainTextEdit(this);
+    /**
+      * @var pteChecksumAdded
+      * A QPlainTextEdit field for the output of the read added Checksum
+      */
+    QPlainTextEdit* pteChecksumAdded = new QPlainTextEdit(this);
+    /**
+      * @var pteChecksummIBM
+      * A QPLainTextEdit field for the output of the read IBM checksum
+      */
+    QPlainTextEdit* pteChecksumIBM = new QPlainTextEdit(this);
+    /**
+      * @var pteMD5
+      * A QPlainTextEdit field for the output of the read MD5
+      */
+    QPlainTextEdit* pteMD5 = new QPlainTextEdit(this);
+
+
+    // ################ //
+    // ## PushButton ## //
+    // ################ //
+    /**
+      * @var pbRefresh
+      * The QPushButton that is used to refresh the information this class
+      * collects and updates the corresponding QPlainTextEdit fields
+      */
+    QPushButton* pbRefresh = new QPushButton(this);
+    /**
+      * @var pbClose
+      * The QPushButton that is used to close this window
+      */
+    QPushButton* pbClose = new QPushButton(this);
+
+private slots:
+    void reload();
+
+};
+
+#endif // CARDINFORMATIONWINDOW_H
